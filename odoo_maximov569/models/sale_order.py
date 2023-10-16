d27246fcfeb04ac338887671e3425f95e966ffcb
Sergey Maximov <maksimov569@gmail.com>
2023-10-17 02:39:22 +0500

    Fix test_field of sale_order model in odoo_maximov569 module.

--- odoo_maximov569/models/sale_order.py
+++ odoo_maximov569/models/sale_order.py
@@ -7,21 +7,24 @@ from odoo.exceptions import ValidationError
 class SaleOrder(models.Model):
     _inherit = 'sale.order'
 
+    @api.model
+    def _generate_ten_random_chars(self) -> str:
+        return ''.join(random.choices(
+            [chr(num) for num in range(97, 123)], k=10)
+        )
+
+    # Also can be used 'size' attribute to set max_length of test_field.
     test_field = fields.Char(
         string='Test',
         readonly=True,
-        states={'draft': [('readonly', False)]}
+        states={'draft': [('readonly', False)]},
+        default=_generate_ten_random_chars
     )
     responsible_for_issuing = fields.Many2one(
         'hr.employee',
         string='Ответственный за выдачу товара',
         required=True,
     )
-
-    def _generate_ten_random_chars(self) -> str:
-        return ''.join(random.choices(
-            [chr(num) for num in range(97, 123)], k=10)
-        )
 
     @api.onchange('date_order', 'order_line')
     def _onchange_date_order_or_product_id(self) -> None:
@@ -31,10 +34,23 @@ class SaleOrder(models.Model):
             else self._generate_ten_random_chars()
         )
 
+    # Onchange can be attached to constrains
+    # but inputted text get missed with validation error.
+    @api.onchange('test_field')
+    def _onchange_check_len_of_test_field(self) -> None or dict[
+                                                           str:dict[str:str]
+                                                           ]:
+        if len(self.test_field) > 50:
+            return {
+                'warning': {
+                    'title': 'Length warning!',
+                    'message': 'Длина текста должна быть меньше 50 символов!',
+                }
+            }
+
     @api.constrains('test_field')
     def _check_test_field(self) -> None or ValidationError:
-        for record in self:
-            if record.test_field and len(record.test_field) > 50:
-                raise ValidationError(
-                    'Длина текста должна быть меньше 50 символов!'
-                )
+        if self.test_field and len(self.test_field) > 50:
+            raise ValidationError(
+                'Длина текста должна быть меньше 50 символов!'
+            )
